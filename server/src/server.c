d99544dc7945ce070a5d5688dfdac1a3dfa6796e
Iliya Iliev <iliyailiev3592@gmail.com>
2020-12-12 10:55:22 +0200

    server: Use generic ll & init connections with fsm
    Signed-off-by: Iliya Iliev <iliyailiev3592@gmail.com>

--- server/src/server.c
+++ server/src/server.c
@@ -2,8 +2,9 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <pthread.h>
-#include <sys/socket.h>
 
+#include "fsm.h"
+#include "util/plf.h"
 #include "util/list.h"
 #include "socket/socket.h"
 
@@ -11,25 +12,42 @@
 #define RX_BUFF_SIZE 256
 
 /******************************* LOCAL TYPEDEFS ******************************/
+typedef struct conn_t {
+    int connfd;
+    fsm_t *fsm;
+    char rxBuffer[RX_BUFF_SIZE];
+} conn_t;
+
 typedef struct server_t {
     int connections;
-    Node_t *connfds;
+    Node_t *conns;
 } server_t;
 
 /********************************* LOCAL DATA *********************************/
 void *connectionHandler(void *arg)
 {
-    int *connfd = 0;
-    char rxBuffer[RX_BUFF_SIZE] = {0};
+    server_t *server = NULL;
 
-    connfd = (int *)arg;
+    server = (server_t *)arg;
+    conn_t conn = *((conn_t *)server->conns->data);
+
+    int connfd = conn.connfd;
+    fsm_t *fsm = conn.fsm;
+    char *rxBuffer = conn.rxBuffer;
 
     for (;;)
     {
-        if(read(*connfd, &rxBuffer, RX_BUFF_SIZE) > 0)
-            printf("%s\n", rxBuffer);
-
-        printf("%d\n", *connfd);
+        if(read(connfd, rxBuffer, RX_BUFF_SIZE) > 0)
+        {
+            fprintf(stdout, "%d\n", connfd);
+            fsm->run(&fsm->state, rxBuffer);
+        }
+        else
+        {
+            fprintf(stderr, "Clossing the connection\n");
+            server->connections--;
+            break;
+        }
     }
 
     return NULL;
@@ -39,21 +57,25 @@ void *connectionHandler(void *arg)
 static void newConnection(int connfd, void *arg)
 {
     server_t *server = NULL;
+    conn_t newConnection;
     pthread_t thread_id;
 
     server = (server_t *)arg;
 
     server->connections++;
-    list_push(&server->connfds, connfd);
-
     fprintf(stdout, "Clients connected %d\n", server->connections);
-    pthread_create(&thread_id, NULL, connectionHandler, &server->connfds->data);
+
+    newConnection.connfd = connfd;
+    newConnection.fsm = getFsm();
+    list_push(&server->conns, &newConnection, DATA_SIZE(conn_t));
+
+    pthread_create(&thread_id, NULL, connectionHandler, server);
 }
 
 static void server_init(server_t *server)
 {
     server->connections = 0;
-    server->connfds = NULL;
+    server->conns = NULL;
 
     return;
 }
